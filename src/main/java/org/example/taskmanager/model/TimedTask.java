1eae52ba9055ac77d451db60ce91106b6158415b
Arjin İLhan <arjinnilhan@gmail.com>
2026-01-05 07:16:17 +0300

    Add TimedTask extending Task

--- src/main/java/org/example/taskmanager/model/TimedTask.java
+++ src/main/java/org/example/taskmanager/model/TimedTask.java
@@ -1,4 +1,24 @@
 package org.example.taskmanager.model;
 
-public class TimedTask {
+public class TimedTask extends Task {
+
+    private int estimatedMinutes;
+
+    public TimedTask(int id,
+                     String title,
+                     Priority priority,
+                     Deadline deadline,
+                     int estimatedMinutes) {
+
+        super(id, title, priority, deadline);
+        this.estimatedMinutes = estimatedMinutes;
+    }
+
+    public int getEstimatedMinutes() {
+        return estimatedMinutes;
+    }
+
+    public void setEstimatedMinutes(int estimatedMinutes) {
+        this.estimatedMinutes = estimatedMinutes;
+    }
 }
